8fd118f263e1410023d72f1c183e9e8d5055ae75
Luis Gustavo Faria <luis.gustavo.fariac@gmail.com>
2023-05-17 21:55:54 -0300

    carregado imagens

--- src/components/characters/Characters.tsx
+++ src/components/characters/Characters.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { MagnifyingGlass, Star } from 'phosphor-react';
 import Image from 'next/image';
 import ironMan from '../../../public/iron-man.png';
@@ -12,6 +12,7 @@ import {
   CardFooter,
   CardImage,
 } from '../characters/styles';
+import { getCharacters } from '../../pages/api/marvels';
 
 interface ICard {
   id: number;
@@ -21,31 +22,43 @@ interface ICard {
 }
 
 export default function Characters() {
-  const cards: ICard[] = [
-    {
-      id: 1,
-      nameCharacter: 'Iron Man',
-      imgCharacter: ironMan,
-      isFavorited: true,
-    },
-    {
-      id: 2,
-      nameCharacter: 'Hulk',
-      imgCharacter: ironMan,
-      isFavorited: true,
-    },
-    {
-      id: 3,
-      nameCharacter: 'Spider Man',
-      imgCharacter: ironMan,
-      isFavorited: false,
-    },
-  ];
+  // const cards: ICard[] = [
+  //   {
+  //     id: 1,
+  //     nameCharacter: 'Iron Man',
+  //     imgCharacter: ironMan,
+  //     isFavorited: true,
+  //   },
+  //   {
+  //     id: 2,
+  //     nameCharacter: 'Hulk',
+  //     imgCharacter: ironMan,
+  //     isFavorited: true,
+  //   },
+  //   {
+  //     id: 3,
+  //     nameCharacter: 'Spider Man',
+  //     imgCharacter: ironMan,
+  //     isFavorited: false,
+  //   },
+  // ];
 
-  const [favoriteCards, setFavoriteCards] = useState<ICard[]>(cards);
+  // const [favoriteCards, setFavoriteCards] = useState<ICard[]>(cards);
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      const data = await getCharacters();
+      if (data) {
+        setCharacters(data);
+      }
+    };
+
+    fetchCharacters();
+  }, []);
 
   const handleFavoriteToggle = (id: Number) => {
-    const newFavoriteCards: ICard[] = favoriteCards.map((card) => {
+    const newFavoriteCards: ICard[] = characters.map((card) => {
       if (card.id === id) {
         return {
           ...card,
@@ -67,15 +80,20 @@ export default function Characters() {
       </DivInput>
 
       <ContainerCards>
-        {favoriteCards.map((card) => (
-          <Card key={card.id}>
+        {characters.map((character) => (
+          <Card key={character.id}>
             <CardImage>
-              <Image src={card.imgCharacter} alt="" priority />
+              <Image
+                src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+                alt={character.name}
+                width={200}
+                height={200}
+              />
             </CardImage>
             <CardFooter>
-              <strong>{card.nameCharacter}</strong>
-              <div onClick={() => handleFavoriteToggle(card.id)}>
-                {card.isFavorited ? (
+              <strong>{character.name}</strong>
+              <div onClick={() => handleFavoriteToggle(character.id)}>
+                {character.isFavorited ? (
                   <Star size={32} color="#ffe357" weight="fill" />
                 ) : (
                   <Star size={32} />
---
+++ src/pages/api/marvels.js
@@ -0,0 +1,20 @@
+import md5 from 'md5';
+
+const publicKey = process.env.NEXT_PUBLIC_MARVEL_PUBLIC_KEY;
+const privateKey = process.env.NEXT_PUBLIC_MARVEL_PRIVATE_KEY;
+const ts = Date.now().toString();
+const hash = md5(`${ts}${privateKey}${publicKey}`);
+
+export const getCharacters = async () => {
+  const url = `https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const characters = data.data.results;
+    return characters;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
