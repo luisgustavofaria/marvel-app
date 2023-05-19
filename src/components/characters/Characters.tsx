e81321caa4a4270274ee009872eed27a9ea5756f
Luis Gustavo <66381613+luisgustavofaria@users.noreply.github.com>
2023-05-19 12:19:13 +0000

    Update handleFavoriteToggle

--- src/components/characters/Characters.tsx
+++ src/components/characters/Characters.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useState } from 'react';
-import { MagnifyingGlass, Star } from 'phosphor-react';
-import Image from 'next/image';
-import ironMan from '../../../public/iron-man.png';
+import { useEffect, useState } from "react";
+import { MagnifyingGlass, Star } from "phosphor-react";
+import Image from "next/image";
 import {
   ContainerDiv,
   DivInput,
@@ -11,40 +10,19 @@ import {
   Card,
   CardFooter,
   CardImage,
-} from '../characters/styles';
-import { getCharacters } from '../../pages/api/marvels';
+} from "../characters/styles";
+import { getCharacters } from "../../pages/api/marvels";
 
-interface ICard {
+export interface ICard {
   id: number;
-  nameCharacter: string;
-  imgCharacter: any;
+  character: string;
+  name: string;
+  thumbnail: any;
   isFavorited: boolean;
 }
 
 export default function Characters() {
-  // const cards: ICard[] = [
-  //   {
-  //     id: 1,
-  //     nameCharacter: 'Iron Man',
-  //     imgCharacter: ironMan,
-  //     isFavorited: true,
-  //   },
-  //   {
-  //     id: 2,
-  //     nameCharacter: 'Hulk',
-  //     imgCharacter: ironMan,
-  //     isFavorited: true,
-  //   },
-  //   {
-  //     id: 3,
-  //     nameCharacter: 'Spider Man',
-  //     imgCharacter: ironMan,
-  //     isFavorited: false,
-  //   },
-  // ];
-
-  // const [favoriteCards, setFavoriteCards] = useState<ICard[]>(cards);
-  const [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState<ICard[]>([]);
 
   useEffect(() => {
     const fetchCharacters = async () => {
@@ -57,8 +35,8 @@ export default function Characters() {
     fetchCharacters();
   }, []);
 
-  const handleFavoriteToggle = (id: Number) => {
-    const newFavoriteCards: ICard[] = characters.map((card) => {
+  const handleFavoriteToggle = (id: number) => {
+    const newFavoriteCards = characters.map((card) => {
       if (card.id === id) {
         return {
           ...card,
@@ -67,7 +45,7 @@ export default function Characters() {
       }
       return card;
     });
-    setFavoriteCards(newFavoriteCards);
+    setCharacters(newFavoriteCards);
   };
 
   return (
